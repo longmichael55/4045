2955c3cd21949d3a16b07195cff1826c4f20afea
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-16 16:37:53 -0500

    Initial Commit

---
+++ src/main/java/com/long2m9_Assignment0/long2m9_Assignment0/Long2m9Assignment0Application.java
@@ -0,0 +1,13 @@
+package com.long2m9_Assignment0.long2m9_Assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Long2m9Assignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Long2m9Assignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/long2m9_Assignment0/long2m9_Assignment0/Long2m9Assignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.long2m9_Assignment0.long2m9_Assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Long2m9Assignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
